116a1366b93e2a05bc90c55250149b83ea59337e
shihabflutter <shihab@aapbd.com>
2021-07-14 10:37:41 +0600

    *one person chat screen done

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final _fireStore = FirebaseFirestore.instance;
+User loggedInUser;
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -10,10 +12,10 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   String messageText;
-  final _fireStore = FirebaseFirestore.instance;
+  final messageTextEditingController = TextEditingController();
 
   final _auth = FirebaseAuth.instance;
-  User loggedInUser;
+
 
   @override
   void initState() {
@@ -40,17 +42,13 @@ class _ChatScreenState extends State<ChatScreen> {
   //   }
   // }
 
-
-  void messagesStream()async{
-
-       await for (var snapshot in _fireStore.collection('messages').snapshots()){
-         for(var message in snapshot.docs){
-           print(message.data());
-         }
-       }
-
+  void messagesStream() async {
+    await for (var snapshot in _fireStore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +72,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            MessageStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -81,6 +80,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextEditingController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -89,6 +89,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   FlatButton(
                     onPressed: () {
+                      messageTextEditingController.clear();
                       _fireStore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedInUser.email,
@@ -105,6 +106,74 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBooble extends StatelessWidget {
+  MessageBooble(this.sender, this.text);
+
+  final String sender;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 10,
+              color: Colors.grey,
+            ),
+          ),
+          Material(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0),bottomLeft: Radius.circular(30.0),bottomRight: Radius.circular(30.0) ),
+            color: Colors.lightBlueAccent,
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ]));
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  //const MessageStream({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _fireStore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        List<MessageBooble> messageWidget = [];
+        if (snapshot.hasData) {
+          final messages = snapshot.data.docs;
+          for (QueryDocumentSnapshot<Map> message in messages) {
+            final messageText = message['text'];
+            final messageSender = message['sender'];
+
+            final messageBooble = MessageBooble(messageSender, messageText);
+            messageWidget.add(messageBooble);
+          }
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            children: messageWidget,
+          ),
+        );
+      },
     );
   }
 }
